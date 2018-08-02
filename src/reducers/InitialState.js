a65689228b3dc2cef715606c013fa9c757ec6fb6
shadome <marceau.imag@gmail.com>
2018-08-02 02:10:14 +0200

    [FIX] added submission fonction

---
+++ src/reducers/InitialState.js
@@ -0,0 +1,30 @@
+// import ListDailyMealsBusiness from "../business/ListDailyMealsBusiness";
+
+// export const InitialState = {
+//   foodName:"toto",
+//   dailyMealsHistory: {
+//     [ListDailyMealsBusiness.getIdFromDate(new Date())]:
+//     [
+//       {
+//         id: '0',
+//         name: 'Banana',
+//         quantity: 200,
+//         unit: 'g',
+//         energy: 77
+//       }, {
+//         id: '1',
+//         name: 'Banana',
+//         quantity: 200,
+//         unit: 'g',
+//         energy: 77
+//       }, {
+//         id: '2',
+//         name: 'Banana',
+//         quantity: 200,
+//         unit: 'g',
+//         energy: 77
+//       }
+//     ]
+//   },
+//   toto:"toto",
+// };

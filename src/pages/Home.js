d2f4352720d71c8817ba2e163dd7b5eb74dccae4
THALGOMA <THALGOMA@PF11-192>
2018-08-06 11:30:38 +0200

    [DRAFT] redux-persist first shot

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,40 @@
 'use strict';
 import React, {Component} from 'react';
 import {NativeModules, StatusBar, View} from 'react-native';
-import {COLOR, ThemeContext, getTheme} from '../components/react-native-material-ui';
+import {COLOR, ThemeContext, getTheme,} from '../components/react-native-material-ui';
 import MainTabNavigator from '../routes';
-import {createStore, applyMiddleware, combineReducers} from 'redux';
-import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, combineReducers,} from 'redux';
+import {Provider,} from 'react-redux';
 import thunk from 'redux-thunk';
 import * as reducers from '../reducers';
+import { persistStore, persistReducer, autoRehydrate, } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const reducer = combineReducers(reducers);
-const store = createStoreWithMiddleware(reducer);
 const iconSet = 'MaterialIcons';
 const palette = {
   primaryColor: COLOR.red500,
   accentColor: COLOR.blue500,
 };
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  stateReconciler: autoMergeLevel2
+};
+const pReducer = persistReducer(persistConfig, reducer);
+const store = createStoreWithMiddleware(pReducer); // const store = createStore(reducer, undefined, autoRehydrate())
+persistStore(store);
 
 class Home extends Component {
   render() {
     return (
-      <ThemeContext.Provider value={getTheme({iconSet, palette})}>
-        <Provider store={store}>
+      <Provider store={store}>
+        <ThemeContext.Provider value={getTheme({iconSet, palette})}>
           <MainTabNavigator ref={(nav) => {this.navigator = nav;}} />
-        </Provider>
-      </ThemeContext.Provider>
+        </ThemeContext.Provider>
+      </Provider>
     );
   }
 }

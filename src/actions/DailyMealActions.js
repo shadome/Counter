4fcbfe2d9f2bed5b1187ffb88344c1731d568b63
THALGOMA <THALGOMA@PF11-192>
2018-08-07 09:19:12 +0200

    [DRAFT] switch to trigger function for DailyMealActions

--- src/actions/DailyMealActions.js
+++ src/actions/DailyMealActions.js
@@ -4,5 +4,7 @@ const prefix = 'DAILY_MEAL';
 export const ADD = '$prefix/ADD';
 export const REMOVE = '$prefix/REMOVE';
 
-export function add(key, meal) { return {type: ADD, key, meal}; }
-export function remove(key, index) { return { type: REMOVE, key, index}; }
+export function trigger(type, ...x) { return {type, x};}
+
+//export function add(key, meal) { return {type: ADD, key, meal}; }
+//export function remove(key, index) { return { type: REMOVE, key, index}; }
